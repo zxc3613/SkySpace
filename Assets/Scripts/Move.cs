4f02895fe445b82b1f87ec32e03707a7e393dd99
Ilkang Oh <zxc3613@naver.com>
2019-11-20 17:51:15 +0900

    지구의 자전, 달의 자전 공전 구현

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,10 +7,17 @@ public class Move : MonoBehaviour
     [SerializeField] Transform earth;
     [SerializeField] Transform month;
 
+    float Earth;
+    float Month;
 
-    // Update is called once per frame
     void Update()
     {
+        Earth += 0.2f;
+        earth.rotation = Quaternion.Euler(0, Earth, 0);
+
+        month.rotation = Quaternion.Euler(0, Earth, 0);
         
+        Month += 1f;
+        transform.rotation = Quaternion.Euler(0, Month, 0);
     }
 }
