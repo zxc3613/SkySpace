92858ffb521e94e292cfc8b1890b3786fe212ebc
Ilkang Oh <zxc3613@naver.com>
2019-11-20 17:29:18 +0900

    우주공간, 지구, 달 표현
    지구의 밤 낮 바뀜 구현

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Transform earth;
+    [SerializeField] Transform month;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Night.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Night : MonoBehaviour
+{
+    void Update()
+    {
+        float val = Mathf.PingPong(Time.time/5, 1);
+        GetComponent<Renderer>().material.SetFloat("_TexValue", val);
+    }
+}
